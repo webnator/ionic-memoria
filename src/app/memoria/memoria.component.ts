bef70ff47a304c310f7b8632906144922fe25ea1
Williams Aguilera <webnator@gmail.com>
2019-03-20 19:47:55 +0100

    ADD: Añadido componente de memoria

---
+++ src/app/memoria/memoria.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memoria',
+  templateUrl: './memoria.component.html',
+  styleUrls: ['./memoria.component.scss'],
+})
+export class MemoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
