88962f85b12388fd48aab413ad53825c5f10e1a6
Williams Aguilera <webnator@gmail.com>
2019-03-21 08:08:16 +0100

    MOD: Añadido el grid con valores al componente memoria

--- src/app/memoria/memoria.component.ts
+++ src/app/memoria/memoria.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memoria.component.scss'],
 })
 export class MemoriaComponent implements OnInit {
-
-  constructor() { }
+  public valores: Array<number>;
+  constructor() { 
+    this.valores = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+  }
 
   ngOnInit() {}
 
