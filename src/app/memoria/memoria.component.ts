e8d9f7e16347de5c80fb01c1f7338cf127837beb
Williams Aguilera <webnator@gmail.com>
2019-03-21 08:13:00 +0100

    MOD: Añadida primera comprobacion de pares

--- src/app/memoria/memoria.component.ts
+++ src/app/memoria/memoria.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemoriaComponent implements OnInit {
   public valores: Array<number>;
+  private seleccionado: number;
   constructor() { 
     this.valores = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+    this.seleccionado = null;
   }
 
   ngOnInit() {}
+
+  public seleccionaValor(valor: number) {
+    if (this.seleccionado === null) {
+      this.seleccionado = valor;
+    } else {
+      if (this.seleccionado === valor) {
+        alert('Conseguiste el par');
+      }
+      this.seleccionado = null;
+    }
+  }
 
 }
