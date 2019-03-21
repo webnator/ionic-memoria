7aecb39a16823e6e059b8e1acc8db94073ce94db
Williams Aguilera <webnator@gmail.com>
2019-03-21 08:22:38 +0100

    ADD: Añadida funcion de escucha en el padre

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,16 @@ export class HomePage {
   public totalAciertos: number;
   public totalIntentos: number;
 
+  constructor() {
+    this.totalAciertos = 0;
+    this.totalIntentos = 0;
+  }
+
+  public chequeaAciertos(acertado: boolean): void {
+    if (acertado) {
+      this.totalAciertos++;
+    }
+    this.totalIntentos++;
+  }
+
 }
