c72f63b5146e64a56d676b0e568acde88827c226
Williams Aguilera <webnator@gmail.com>
2019-03-23 11:51:20 +0100

    MOD: Refactorizada la condicion de visibilidad

--- src/app/memoria/memoria.component.ts
+++ src/app/memoria/memoria.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class MemoriaComponent implements OnInit {
   @Output() acertado: EventEmitter<boolean> = new EventEmitter<boolean>();
   public valores: Array<number>;
+  public aciertos: Array<number>;
   private seleccionado: number;
   constructor() { 
     this.valores = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
     this.seleccionado = null;
+    this.aciertos = [];
   }
 
   ngOnInit() {}
@@ -22,12 +24,20 @@ export class MemoriaComponent implements OnInit {
     } else {
       if (index === this.seleccionado) { return; }
       if (this.valores[this.seleccionado] === this.valores[index]) {
+        // guardamos los indices de los valores que se han acertado
+        this.aciertos.push(this.seleccionado);
+        this.aciertos.push(index);
+
         this.acertado.emit(true);
       } else {
         this.acertado.emit(false);
       }
       this.seleccionado = null;
     }
+  }
+
+  public esVisible(index: number): boolean {
+    return this.seleccionado === index;
   }
 
 }
