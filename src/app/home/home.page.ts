2b721110c542485ef2476261831b0a20bffa0193
Williams Aguilera <webnator@gmail.com>
2019-04-07 20:33:37 +0200

    ADD: Añadida redireccion en caso de que no haya jugadores

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ResultadoTurno } from '../memoria/resultado-turno';
 import { Jugador } from './jugador';
 import { JugadoresService } from '../jugadores.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,15 @@ export class HomePage {
   public jugadores: Array<Jugador> = [];
   public jugadorActual: Jugador;
 
-  constructor(private jugadoresService: JugadoresService) {
+  constructor(private jugadoresService: JugadoresService, private router: Router) {
     this.inicializaJugadores();
   }
 
   private inicializaJugadores() {
     this.jugadores = this.jugadoresService.jugadores;
+    if (this.jugadores.length <= 0) {
+      this.router.navigate(['/inicio']);
+    }
     this.jugadorActual = this.jugadores[0];
   } 
 
